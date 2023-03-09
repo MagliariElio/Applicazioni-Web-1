791577c9b466231dd796ee491ad30529d7115c21
MagliariElio <magliari.elio08@gmail.com>
2023-03-09 21:11:34 +0100

    database access

--- EXERCISE/EXERCISE_6/exercise_6.js
+++ EXERCISE/EXERCISE_6/exercise_6.js
@@ -1,9 +1,18 @@
 "use strict";
 
 
+console.log("This is an example of database access");
+
 const sqlite = require("sqlite3");
 const db = new sqlite.Database('exams.sqlite', (err) => {if(err) throw err;});
 
+const sql = "SELECT * FROM course";
+db.all(sql, (err, rows) => {
+    if(err) throw new err;
+    for(let row of rows) {
+        console.log(row);
+    }
+});
 
 
 db.close();
