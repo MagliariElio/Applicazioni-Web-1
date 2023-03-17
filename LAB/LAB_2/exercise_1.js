65ea58bb2ae47dabea2e5dbb5202a728089fe5a9
MagliariElio <magliari.elio08@gmail.com>
2023-03-17 17:53:19 +0100

    lab2

--- LAB/LAB_2/exercise_1.js
+++ LAB/LAB_2/exercise_1.js
@@ -2,7 +2,7 @@
 
 const dayjs = require('dayjs');
 const sqlite = require("sqlite3");
-const db = new sqlite.Database('films.sqlite', (err) => {if(err) throw err;});
+const db = new sqlite.Database('films.sqlite', (err) => { if (err) throw err; });
 
 
 function Film(id, title, isFavourite, watchDate, score) {
@@ -27,7 +27,7 @@ function FilmLibrary() {
     this.sortByDate = () => {
         let list = [...this.listFilms].filter((a) => a.watchDate !== undefined);
         let listUnwatched = [...this.listFilms].filter((a) => a.watchDate === undefined);
-        list = list.sort((a, b) => (dayjs(a.watchDate).isAfter(b.watchDate) ? 1 : -1)).concat(listUnwatched); 
+        list = list.sort((a, b) => (dayjs(a.watchDate).isAfter(b.watchDate) ? 1 : -1)).concat(listUnwatched);
         return list;
     }
 
@@ -49,31 +49,85 @@ function FilmLibrary() {
 
     this.getAllFilms = async () => {
         return new Promise((resolve, reject) => {
-            const sql = "SELECT * FROM films";
+            const sql = "SELECT id, title, favorite, watchdate, rating FROM films";
+            let result = [];
 
             db.all(sql, (err, rows) => {
-                if(err) reject(err.message);
+                if (err) reject(err);
                 else {
-                    for(let row of rows) {
-                        this.addNewFilm(new Film(row.id, row.title, row.favorite, dayjs(row.watchdate), row.rating));
+                    for (let row of rows) {
+                        result.push(new Film(row.id, row.title, row.favorite, dayjs(row.watchdate), row.rating));
                     }
-                    resolve(this.listFilms);
+                    resolve(result);
                 }
             });
+        })
+    }
+
+    this.getAllFavoriteFilms = async () => {
+        return new Promise((resolve, reject) => {
+            const sql = "SELECT id, title, favorite, watchdate, rating FROM films WHERE favorite = 1";
+            let result = [];
+
+            db.all(sql, (err, rows) => {
+                if (err) reject(err);
+                else {
+                    for (let row of rows) {
+                        result.push(new Film(row.id, row.title, row.favorite, dayjs(row.watchdate), row.rating));
+                    }
+                    resolve(result);
+                }
+            })
+        })
+    }
+
+    this.getAllFilmWatchedToday = async () => {
+        let today = dayjs("2023-03-10");    // andrebbe personalizzato per il giorno odierno ma per semplicità supponiamo che sia questo
+        let result = await this.getAllFilms();
+        return result.filter((film) => dayjs(film.watchDate).isSame(today));
+    }
+
+    this.getAllFilmWatchedOnSomeDate = async (date) => {
+        return new Promise((resolve, reject) => {
+            const sql = "SELECT id, title, favorite, watchdate, rating FROM films WHERE watchdate < ?";
+            let result = [];
+
+            db.all(sql, [date.format("YYYY-MM-DD")], (err, rows) => {
+                if (err) reject(err);
+                for (let row of rows) {
+                    result.push(new Film(row.id, row.title, row.favorite, dayjs(row.watchdate), row.rating));
+                }
+                resolve(result);
+
+            })
         })
     }
 }
 
 
 
-function main() {
+async function main() {
     const library = new FilmLibrary();
 
+    /*console.log("Stampa di tutti i film");
     library.getAllFilms()
         .then(library.print)
+        .catch((e) => console.error("Errore gestito: " + e.message));*/
+
+    /*console.log("Stampa di tutti i film preferiti");
+    library.getAllFavoriteFilms()
+        .then(library.print)
+        .catch((e) => console.error("Errore gestito: " + e.message));*/
+
+    /*console.log("Stampa di tutti i film guardati oggi");
+    library.getAllFilmWatchedToday()
+        .then(library.print)
+        .catch((e) => console.error("Errore gestito: " + e.message));*/
+
+    console.log("Stampa di tutti i film visti prima di oggi");
+    library.getAllFilmWatchedOnSomeDate(dayjs("2023-03-17"))
+        .then(library.print)
         .catch((e) => console.error("Errore gestito: " + e.message));
-
-
 }
 
 main();
